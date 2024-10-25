55eae3883ede8760144c025bb500645fb408ac14
bekishh <beknazarzholdoshbekov1@gmail.com>
2024-10-25 10:27:24 +0600

    Reconfigure settings

--- auto_commit.sh
+++ auto_commit.sh
@@ -3,14 +3,22 @@
 # Переходим в директорию репозитория
 cd /home/beqishh/push-flow || exit
 
-# Записываем текущую дату и время в файл
-echo "$(date)" >> auto_update.txt
+# Генерация случайного количества коммитов (от 65 до 76)
+commits_today=$((65 + RANDOM % 12))
 
-# Добавляем все изменения
-git add .
+# Цикл для выполнения нужного количества коммитов
+for ((i=1; i<=commits_today; i++))
+do
+    # Записываем текущую дату и время в файл
+    echo "$(date) - Commit $i of $commits_today" >> auto_update.txt
 
-# Коммитим изменения с текущей датой и временем
-git commit -m "Auto commit on $(date)"
+    # Добавляем все изменения
+    git add .
+
+    # Коммитим изменения
+    git commit -m "Auto commit $i of $commits_today on $(date)"
+
+done
 
 # Пушим изменения в удаленный репозиторий
 git push origin main
