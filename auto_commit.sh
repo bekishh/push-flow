d573637527f241b5f39cf72c0ef1921e149e4946
bekishh <beknazarzholdoshbekov1@gmail.com>
2024-10-25 10:05:10 +0600

    Auto commit on Пт 25 окт 2024 10:05:10 +06

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Переходим в директорию репозитория
+cd /home/beqishh/push-flow || exit
+
+# Записываем текущую дату и время в файл
 echo "$(date)" >> auto_update.txt
 
+# Добавляем все изменения
 git add .
 
+# Коммитим изменения с текущей датой и временем
 git commit -m "Auto commit on $(date)"
 
+# Пушим изменения в удаленный репозиторий
 git push origin main
